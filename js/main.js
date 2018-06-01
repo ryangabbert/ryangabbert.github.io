f4f1e4ad5067d74130eb44b9fbf83ae95b3aef25
Ryan Gabbert <rgabb08@gmail.com>
2018-06-01 00:03:10 -0400

    made major changes again

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 /* General jQuery & JS stuff goes here. */
 /* General jQuery & JS stuff goes here. */
 jQuery(document).ready(function ($) {
-    console.log("A starter template created by Eleven Fifty Academy under MIT Licensing.");
     /* Initialize wowjs */
     new WOW().init();
     });
